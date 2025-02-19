13f34c9c377e9605309c3c7f08b04b1b67818020
rudrasantadip <santadip9@gmail.com>
2025-02-19 12:14:42 -0500

    taught filter

--- Main.java
+++ Main.java
@@ -1,103 +1,53 @@
-import java.util.Optional;
+import java.util.List;
 
-interface Calculator{
-    public Integer add(int a, int b);
-    public Integer sub(int a, int b);
-    public Integer mul(int a, int b);
-    public Float div(int a, int b);
+class User
+{
+private String username;
+private Integer age;
+
+public User(String username, Integer age)
+{
+this.username=username;
+this.age=age;
+}
+
+public Integer getAge()
+{
+return age;
+}
+
+public String getUserName()
+{
+return username;
+}
+
+@Override
+public String toString()
+{
+return ("Customer { name: "+ username+" Age: "+age+" }");
+}
+
 }
 
 
-interface  OptionalCalculator {
-    public Optional<Integer> add(int a, int b);
-    public Optional<Integer> sub(int a, int b);
-    public Optional<Integer> mul(int a, int b);
-    public Optional<Float> div(int a, int b);
-}
-
-class BasicCalculator implements Calculator
+public class Main
 {
 
-    @Override
-    public Integer add(int a, int b) {
-        return a+b;
-    }
-
-    @Override
-    public Integer sub(int a, int b) {
-        return a-b;
-    }
-
-    @Override
-    public Integer mul(int a, int b) {
-        return a*b;
-    }
-
-    @Override
-    public Float div(int a, int b) {
-        if(b==0)
-        {
-            return null;
-        }
-        return (float)a/b;
-    }
-
-}
-
-
-class  OptionalCalculatorImpl implements OptionalCalculator
+public static  List<User> getUsers()
 {
-
-    @Override
-    public Optional<Integer> add(int a, int b) {
-        return Optional.of(a+b);
-    }
-
-    @Override
-    public Optional<Integer> sub(int a, int b) {
-        return Optional.of(a-b);
-    }
-
-    @Override
-    public Optional<Integer> mul(int a, int b) {
-       return Optional.of(a*b);
-    }
-
-    @Override
-    public Optional<Float> div(int a, int b) {
-       return b==0?Optional.empty():Optional.of((float)a/b);
-    }
-
-    private OptionalCalculatorImpl()
-    {
-
-    }
-
-    public static OptionalCalculatorImpl getInstance()
-    {
-        return new OptionalCalculatorImpl();
-    }
+return List.of(
+new User("A",18),
+new User("B",21),
+new User("C",23),
+new User("D",25),
+new User("E",10)
+);
 }
 
 
-public class Main{
-    public static void main(String[] args)  {
-       OptionalCalculator calculator = OptionalCalculatorImpl.getInstance();
-       Float result;
-     
-    try {
-        result = calculator.div(5, 0).orElseThrow(
-            ()-> 
-            {
-            return new Exception("Some error occured");
-            }
-          
-        );
-        System.out.println(result);
-    } catch (Exception e) {  
-        System.out.println(e.getMessage());
-    }
-
-  
-    }
+public static void main(String args[])
+{
+	List<User> users = getUsers();
+	users.stream().filter(u->u.getAge()>18).forEach(System.out::println);
+}
 }
